0a11d4c1b68ce2ca2170ef580b1e5b68dd0390d6
Oscar Castillo <goldoac@live.com>
2022-10-22 06:14:22 -0600

    Initial Commit

---
+++ VideoCaster/VideoCasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCasterApp.swift
+//  VideoCaster
+//
+//  Created by Oscar Castillo on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoCasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
